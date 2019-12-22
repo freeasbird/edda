48d8a3dc756e0146a1156a884792e784dc410d3a
offer365 <332000640@qq.com>
2019-12-22 16:33:58 +0800

    updage

--- eddaX/eddaX.go
+++ eddaX/eddaX.go
@@ -1,7 +1,11 @@
 package eddaX
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
+	"io"
+	"io/ioutil"
 	"reflect"
 
 	"github.com/offer365/edda/log"
@@ -88,4 +92,35 @@ func Start(cfg *Config) {
 	Cfg = cfg
 	Main()
 	return
+}
+
+
+type Result struct {
+	SerialNum string          `json:"serial_num"`
+	Apps      map[string]*App `json:"apps"`
+}
+
+func GenAuth(body io.Reader) (code string, err error) {
+	var authresp *AuthResp
+	byt, err := ioutil.ReadAll(body)
+	if err != nil {
+		return
+	}
+	result := new(Result)
+	err = json.Unmarshal(byt, result)
+	if err != nil {
+		return
+	}
+	cipher := &Cipher{
+		Code: result.SerialNum,
+	}
+	ar := &AuthReq{
+		Cipher: cipher,
+		Apps:   result.Apps,
+	}
+	if authresp, err = AuthServer.Authorized(context.Background(), ar); err != nil {
+		return
+	}
+	return authresp.Cipher.Code, err
+
 }
--- eddaX/server.go
+++ eddaX/server.go
@@ -1,14 +1,18 @@
 package eddaX
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"errors"
@@ -34,6 +38,26 @@ var (
 func args() {
 	flag.StringVar(&ListenAddr, "l", ":19527", "listen addr.")
 	flag.Parse()
+}
+
+// 释放静态资源
+func RestoreAsset() {
+	// 解压 静态文件的位置
+	if runtime.GOOS == "linux" {
+		AssetPath = "/usr/share/.asset/.temp/"
+	} else {
+		AssetPath = "./"
+	}
+	// go get -u github.com/jteeuwen/go-bindata/...
+	// 重新生成静态资源在项目的根目录下 go-bindata -o=asset/asset.go -pkg=asset views/... static/...
+	dirs := []string{"views", "static"}
+	for _, dir := range dirs {
+		if err := asset.RestoreAssets(AssetPath, dir); err != nil {
+			log.Sugar.Error("RestoreAssets error. ", err.Error())
+			_ = os.RemoveAll(filepath.Join(AssetPath, dir))
+			continue
+		}
+	}
 }
 
 func init() {
@@ -136,22 +160,160 @@ func route() http.Handler {
 	return r
 }
 
-// 释放静态资源
-func RestoreAsset() {
-	// 解压 静态文件的位置
-	if runtime.GOOS == "linux" {
-		AssetPath = "/usr/share/.asset/.temp/"
-	} else {
-		AssetPath = "./"
+
+func UntiedApi(c *gin.Context) {
+	var (
+		app, id string
+	)
+
+	app = c.Param("app")
+	id = c.Param("id")
+	req := UntiedReq{
+		App: app,
+		Id:  id,
 	}
-	// go get -u github.com/jteeuwen/go-bindata/...
-	// 重新生成静态资源在项目的根目录下 go-bindata -o=asset/asset.go -pkg=asset views/... static/...
-	dirs := []string{"views", "static"}
-	for _, dir := range dirs {
-		if err := asset.RestoreAssets(AssetPath, dir); err != nil {
-			log.Sugar.Error("RestoreAssets error. ", err.Error())
-			_ = os.RemoveAll(filepath.Join(AssetPath, dir))
-			continue
+	cipher, err := AuthServer.Untied(context.TODO(), &req)
+	if err != nil {
+		c.JSON(401, map[string]string{"code": "error"})
+		return
+	}
+	c.JSON(200, map[string]string{"code": cipher.Code})
+}
+
+// 应用
+func AppAPI(c *gin.Context) {
+	var (
+		id string
+	)
+
+	id = c.Param("id")
+	id = strings.Trim(id, "/")
+	page, err := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
+	if err != nil {
+		page = 1
+	}
+	if page <= 0 {
+		page = 1
+	}
+	size, err := strconv.ParseInt(c.DefaultQuery("size", "10"), 10, 64)
+	if err != nil {
+		size = 10
+	}
+	if size <= 0 || size > 100 {
+		size = 10
+	}
+
+	switch c.Request.Method {
+	case "PUT":
+		id, err := InsertApp(c.Request.Body)
+		if err != nil {
+			c.JSON(200, gin.H{"code": 404, "msg": err.Error(), "data": nil})
+			return
 		}
+		c.JSON(200, gin.H{"code": 200, "msg": "success", "data": id})
+		return
+	case "GET":
+		// one
+		if id != "" {
+			_id, err := strconv.Atoi(id)
+			data := FindOneApp(_id)
+			if err != nil {
+				c.JSON(200, gin.H{"code": 404, "msg": err.Error(), "data": nil})
+				return
+			}
+			c.JSON(200, gin.H{"code": 200, "msg": "success", "data": data})
+			return
+		}
+		// many
+		data := FindAllApp()
+		if err != nil {
+			c.JSON(200, gin.H{"code": 404, "msg": err.Error(), "data": nil})
+			return
+		}
+		c.JSON(200, gin.H{"code": 200, "msg": "success", "data": data})
+		return
+	case "DELETE":
+		_id, err := strconv.Atoi(id)
+		DeleteApp(_id)
+		if err != nil {
+			c.JSON(200, gin.H{"code": 404, "msg": err.Error(), "data": nil})
+			return
+		}
+		c.JSON(200, gin.H{"code": 200, "msg": "success", "data": nil})
+		return
+	case "POST":
+		_id, err := strconv.Atoi(id)
+		UpdateApp(_id, c.Request.Body)
+		if err != nil {
+			c.JSON(200, gin.H{"code": 404, "msg": err.Error(), "data": nil})
+			return
+		}
+		c.JSON(200, gin.H{"code": 200, "msg": "success", "data": nil})
+		return
+	default:
+		c.JSON(200, gin.H{
+			"code": 1,
+			"msg":  "Method error.",
+		})
+	}
+
+}
+
+func LicenseAPI(c *gin.Context) {
+	if code, err := GenAuth(c.Request.Body); err == nil {
+		c.JSON(200, gin.H{"code": 200, "data": code})
+	}
+}
+
+func ServerAPI(c *gin.Context) {
+	data, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+
+		return
+	}
+	ctx := context.TODO()
+	switch c.Param("do") {
+	case "resolved":
+		req := new(Cipher)
+		err = json.Unmarshal(data, req)
+		if err != nil {
+			return
+		}
+
+		resp, err := AuthServer.Resolved(ctx, req)
+		c.JSON(200, gin.H{"serial": resp, "msg": err})
+		return
+	case "authorized":
+		req := new(AuthReq)
+		err = json.Unmarshal(data, req)
+		if err != nil {
+			return
+		}
+
+		resp, err := AuthServer.Authorized(ctx, req)
+		c.JSON(200, gin.H{"auth": resp, "msg": err})
+		return
+	case "untied":
+		req := new(UntiedReq)
+		err = json.Unmarshal(data, req)
+		if err != nil {
+			return
+		}
+
+		resp, err := AuthServer.Untied(ctx, req)
+		c.JSON(200, gin.H{"cipher": resp, "msg": err})
+		return
+	case "cleared":
+		req := new(Cipher)
+		err = json.Unmarshal(data, req)
+		if err != nil {
+			return
+		}
+
+		resp, err := AuthServer.Cleared(ctx, req)
+		c.JSON(200, gin.H{"clear": resp, "msg": err})
+		return
+	default:
+		c.JSON(404, nil)
 	}
 }
